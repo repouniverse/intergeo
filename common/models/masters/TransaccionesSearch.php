d545f9306a557d18ccdd27791e869ecc406a617a
Julian Ramirez <hipogea@hotmail.com>
2020-07-20 16:16:34 -0500

    prueba pequeña

--- common/models/masters/TransaccionesSearch.php
+++ common/models/masters/TransaccionesSearch.php
@@ -21,7 +21,9 @@ class TransaccionesSearch extends Transacciones
             [['name', 'esruta', 'grupo','transaccion'], 'safe'],
         ];
     }
-
+/*
+ * comentario
+ */
     /**
      * {@inheritdoc}
      */
